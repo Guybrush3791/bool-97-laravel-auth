295a94ee7a7af4e31508bbbbd9b49e1ec9080606
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2024-04-23 05:49:17 +0200

    init

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,52 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\AuthApiController;
+use App\Http\Controllers\Api\AdminApiController;
+use App\Http\Controllers\Api\UserApiController;
+use App\Http\Controllers\Api\GuestApiController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// AUTH
+Route :: post('login', [AuthApiController::class, 'login']) -> name('login');
+Route :: post('register', [AuthApiController::class, 'register']) -> name('register');
+
+
+// LOGGED USER
+Route::middleware('auth:api')->group(function () {
+
+    // ADMIN ONLY
+    Route::middleware(['role:admin'])->prefix('admin')->group(function () {
+
+        // IS ADMIN
+        Route::get('is-admin', [AdminApiController::class, 'isAdmin']);
+
+        // GET USERS
+        Route::get('users', [AdminApiController::class, 'getUsers']);
+        // ADD USER
+        Route::post('users', [AdminApiController::class, 'addUser']);
+        // UPDATE USER
+        Route::put('users/{id}', [AdminApiController::class, 'updateUser']);
+        // DELETE USER
+        Route::delete('users/{id}', [AdminApiController::class, 'deleteUser']);
+    });
+
+    Route :: prefix('user') -> group(function () {
+        // ME
+        Route :: get('me', [AuthApiController::class, 'me']);
+        Route :: put('update', [AuthApiController::class, 'update']) -> name('update');
+
+        // GET MY BOOKS
+        Route :: get('books', [UserApiController::class, 'getBooks']);
+        // GET MY BOOK BY ID
+        Route :: get('books/{id}', [UserApiController::class, 'getBook']);
+        // ADD BOOK
+        Route :: post('books', [UserApiController::class, 'addBook']);
+        // UPDATE BOOK
+        Route :: put('books/{id}', [UserApiController::class, 'updateBook']);
+        // DELETE BOOK
+        Route :: delete('books/{id}', [UserApiController::class, 'deleteBook']);
+    });
 });
+
+// GUEST
+Route::get('books', [GuestApiController::class, 'getBooks']);
--- routes/web.php
+++ routes/web.php
@@ -7,26 +7,30 @@ use App\Http\Controllers\GuestController;
 use App\Http\Controllers\LoggedController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('backend');
 }) -> name('home');
 
-Route::get('/dashboard', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// }) -> name('home');
 
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route :: get('/guest/home', [GuestController :: class, 'index'])
-    -> name('guest.index');
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
-Route :: get('/logged/home', [LoggedController :: class, 'index'])
-    -> middleware('auth')
-    -> name('logged.index');
+// Route :: get('/guest/home', [GuestController :: class, 'index'])
+//     -> name('guest.index');
+
+// Route :: get('/logged/home', [LoggedController :: class, 'index'])
+//     -> middleware('auth')
+//     -> name('logged.index');
 
 // Route::middleware('auth')
 //     -> name('logged.')
